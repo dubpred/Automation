6da692b7efa2e7a8ab3f00502e22a6664d89f39e
unknown <pl73775@PP7949C9H2.pncbank.com>
2017-09-22 10:27:34 -0400

    Initial MouseMover Class Creation

---
+++ src/automationTests/MouseMover.java
@@ -0,0 +1,12 @@
+package automationTests;
+
+import java.awt.Robot;
+
+
+public class MouseMover {
+	
+	public static void main(String[] args) throws Exception{
+		Robot robot = new Robot();
+	}
+
+}
